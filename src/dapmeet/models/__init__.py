3f6947b7609091846fe86459d784fbef105b44e4
Abdussalam Abdurakhimov <abduss@Abdussalams-MacBook-Air.local>
2025-06-21 15:33:24 +0500

    alembic

--- src/dapmeet/models/__init__.py
+++ src/dapmeet/models/__init__.py
@@ -1,6 +1,6 @@
 import importlib, pkgutil
 import dapmeet.models as models_pkg
-
+from dapmeet.db.db import Base
 for _, module_name, _ in pkgutil.iter_modules(models_pkg.__path__):
     importlib.import_module(f"dapmeet.models.{module_name}")
 
