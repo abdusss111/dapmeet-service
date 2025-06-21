44eb725cb7592a3bf2df27746b0b7c1358aa53af
Abdussalam Abdurakhimov <abduss@Abdussalams-MacBook-Air.local>
2025-06-21 15:16:26 +0500

    Transcript Segment

--- alembic/env.py
+++ alembic/env.py
@@ -4,8 +4,8 @@ from alembic import context
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from db import Base  # ✅ import Base
-from models import *  # ✅ import your models
+from dapmeet.db.db import Base  # ✅ import Base
+from dapmeet.db.models import *  # ✅ import your models
 
 config = context.config
 fileConfig(config.config_file_name)
