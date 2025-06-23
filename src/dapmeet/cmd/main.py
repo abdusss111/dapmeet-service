3ce70ffdfc86fc6a3381cfb58bb8472dc1187f57
Abdussalam Abdurakhimov <abduss@Abdussalams-MacBook-Air.local>
2025-06-23 09:39:39 +0500

    Added google meet to allowed cors hosts

--- src/dapmeet/cmd/main.py
+++ src/dapmeet/cmd/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://dapmeet.kz"],
+    allow_origins=["https://dapmeet.kz", "https://meet.google.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
