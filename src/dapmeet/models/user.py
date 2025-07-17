ebaf9100f84fc6375b3b2112d4caff8d920b1998
Abdussalam Abdurakhimov <abduss@Abdussalams-MacBook-Air.local>
2025-07-17 17:16:54 +0500

    reorganized models

--- src/dapmeet/models/user.py
+++ src/dapmeet/models/user.py
@@ -12,7 +12,7 @@ class User(Base):
     name = Column(String(100), nullable=True)
     created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
 
-    meet ings = relationship(
+    meetings = relationship(
         "Meeting", back_populates="user",
         cascade="all, delete-orphan"
     )
