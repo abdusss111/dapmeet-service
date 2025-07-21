abde0038cec1bd5df0286f1c36f37b07bc27d754
Abdussalam Abdurakhimov <abduss@Abdussalams-MacBook-Air.local>
2025-07-21 21:17:34 +0600

    revert

--- src/dapmeet/db/db.py
+++ src/dapmeet/db/db.py
@@ -8,4 +8,3 @@ DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localho
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
